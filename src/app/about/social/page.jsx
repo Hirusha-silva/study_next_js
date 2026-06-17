501a1e299c7d2529868305296d1a2f308bf7e818
Hirusha-silva <hirushasilva222@gmail.com>
2026-06-17 19:24:06 +0530

    routing and nested routing

---
+++ src/app/about/social/page.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react'
+
+const page = () => {
+  return (
+    <h1>social</h1>
+  )
+}
+
+export default page
---
+++ src/app/products/page.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react'
+
+const page = () => {
+  return (
+    <h1>Product Page</h1>
+  )
+}
+
+export default page
